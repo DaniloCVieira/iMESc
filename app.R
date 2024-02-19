e280d5aff0aed34225e376e62ed5c27b4cac953d
DaniloCVieira <vieiradc@yahoo.com.br>
2024-02-19 18:24:48 -0300

    update files

--- app.R
+++ app.R
@@ -1,11 +1,11 @@
-
+options(shiny.autoload.r=FALSE)
 list.of.packages <- c('remotes')
 if(!length(grep("connect/apps",getwd()))>0){
   new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
   if(length(new.packages)) {install.packages(new.packages, dependencies = TRUE)}
   remotes::install_deps(upgrade="never")
 }
-options(shiny.autoload.r=FALSE)
+
 pkgload::load_all(export_all = T,quiet =T,warn_conflicts =F)
 
 run_app(options=list(quiet=T,shiny.autoload.r=FALSE, launch.browser=T))

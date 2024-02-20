ae150f114878b55bb1b8418626029dde6ad251be
DaniloCVieira <vieiradc@yahoo.com.br>
2024-02-20 06:38:50 -0300

    update files

--- app.R
+++ app.R
@@ -1,20 +1,17 @@
 
-suppressWarnings({
+
+list.of.packages <- c('remotes')
+if(!length(grep("connect/apps",getwd()))>0){
+  new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+  if(length(new.packages)) {install.packages(new.packages, dependencies = TRUE)}
+  remotes::install_deps("inst",upgrade="never")
+}
+suppressPackageStartupMessages(pkgload::load_all("inst",export_all = T,quiet =T,warn_conflicts =F))
 
 
-  options(shiny.autoload.r=FALSE)
-  list.of.packages <- c('remotes')
-  if(!length(grep("connect/apps",getwd()))>0){
-    new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-    if(length(new.packages)) {install.packages(new.packages, dependencies = TRUE)}
-    remotes::install_deps("inst",upgrade="never")
-  }
+shinyApp(app_ui,app_server)
 
-  pkgload::load_all("inst",export_all = T,quiet =T,warn_conflicts =F)
 
-  shinyApp(app_ui,app_server)
-
-})
 
 #shiny::runGitHub("iMESc","DaniloCVieira","main")
 #shiny::shinyApp(app_ui, app_server)
